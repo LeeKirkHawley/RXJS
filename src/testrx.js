160c7b375d419654482402df1add0c9476c33f4b
Kirk Hawley <leekirkhawley@gmail.com>
2023-02-19 14:04:32 -0700

    more code

--- src/testrx.js
+++ src/testrx.js
@@ -3,7 +3,7 @@
 
 
 import { of } from 'rxjs';
-import {mapTo, take, tap, map} from "rxjs/operators"
+import {reduce, filter, take, tap, map} from "rxjs/operators"
 import { Observable } from 'rxjs'
 import { Subject } from 'rxjs'
 
@@ -31,3 +31,10 @@ observable.subscribe({
   complete: () => console.info('complete') 
 })
 
+
+let test1 = of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+let case1 = test1.pipe(
+   filter(x => x % 2 === 0),
+   reduce((acc, one) => acc + one, 0)
+)
+case1.subscribe(x => console.log(x));
